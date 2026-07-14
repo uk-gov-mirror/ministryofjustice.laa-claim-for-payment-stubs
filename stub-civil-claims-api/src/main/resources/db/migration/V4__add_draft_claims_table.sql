ed16530f9d353fcc3174128b631b92e7e24e3e32
Adam Coles <adam.coles@justice.gov.uk>
2026-07-14 16:11:57 +0100

    fixed v4 flyway update

--- stub-civil-claims-api/src/main/resources/db/migration/V4__add_draft_claims_table.sql
+++ stub-civil-claims-api/src/main/resources/db/migration/V4__add_draft_claims_table.sql
@@ -1,5 +1,3 @@
--- Enable UUID generation in Postgres (ignored by H2)
-
 CREATE TABLE IF NOT EXISTS draft_claims (
       id                  UUID PRIMARY KEY,
       payload             JSONB NOT NULL,
